ac507c55bbabc2d54327b03a6f5e1bc12e5269a7
WILLIAM COLASSO PEREIRA <william_c_pereira@estudante.sesisenai.org.br>
2024-11-22 14:09:32 -0300

    Inicialização das dependências do projeto e esqueleto da arquitetura

---
+++ app_adapter/src/main/java/com/psii/app_adapter/AppAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.psii.app_adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAdapterApplication.class, args);
+	}
+
+}
---
+++ app_adapter/src/test/java/com/psii/app_adapter/AppAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psii.app_adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
