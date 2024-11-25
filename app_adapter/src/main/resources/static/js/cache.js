c8d27104f67ef26c611e2e114a39237d6f4b7ac7
WILLIAM COLASSO PEREIRA <william_c_pereira@estudante.sesisenai.org.br>
2024-11-25 13:46:56 -0300

    side bar

---
+++ app_adapter/src/main/resources/static/js/cache.js
@@ -0,0 +1,24 @@
+// Verifica se o usuário está logado
+function verificarLogin() {
+    const usuarioLogado = localStorage.getItem('usuarioLogado');
+    const dadosUsuario = localStorage.getItem('dadosUsuario');
+
+    if (!usuarioLogado || !dadosUsuario) {
+        // Se não existir, cria variáveis no localStorage
+        localStorage.setItem('usuarioLogado', 'false');
+        localStorage.setItem('dadosUsuario', JSON.stringify({}));
+
+        // Redireciona para a página de criação de conta
+        window.location.href = '/geral.html';
+    } else {
+        const logado = localStorage.getItem('usuarioLogado') === 'true';
+        
+        if (!logado) {
+            // Caso não esteja logado, redireciona
+            window.location.href = '/geral.html';
+        }
+    }
+}
+
+// Chama a função para verificar o estado do login
+verificarLogin();
