8f35b61b3e1bad70915fd3b2fdc297e171ee405c
William Colasso <william.colasso@gmail.com>
2024-11-25 22:15:44 -0300

    Barra superior + tela de login

--- app_adapter/src/main/java/com/psii/app_adapter/Controller/ContaController.java
+++ app_adapter/src/main/java/com/psii/app_adapter/Controller/ContaController.java
@@ -3,6 +3,8 @@ package com.psii.app_adapter.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.psii.app_adapter.Model.Conta;
@@ -11,12 +13,20 @@ import com.psii.app_adapter.Service.ContaService;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/contas")
+@Controller
+@RequestMapping
 public class ContaController {
 
     @Autowired
     private ContaService contaService;
 
+
+
+    @GetMapping("conta/cadastro/{id}")
+    public String getCadastro(@PathVariable Long id, Model model){
+        
+
+        return "conta/cadastro";
+    }
     
 }
