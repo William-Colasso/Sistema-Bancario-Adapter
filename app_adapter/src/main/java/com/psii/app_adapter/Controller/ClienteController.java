c3415c6332c516e818bd91047d6e64ac39e3c4c7
William Colasso <william.colasso@gmail.com>
2024-11-28 15:46:34 -0300

    Perfil pronto

--- app_adapter/src/main/java/com/psii/app_adapter/Controller/ClienteController.java
+++ app_adapter/src/main/java/com/psii/app_adapter/Controller/ClienteController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.psii.app_adapter.Model.Cliente;
-
 import com.psii.app_adapter.Service.ClienteService;
 
 import java.util.List;
@@ -32,27 +31,21 @@ public class ClienteController {
 
     @PostMapping("/clientes")
     public String postClientes(Cliente cliente) {
-
         clienteService.createCliente(cliente);
         return "redirect:/home";
     }
 
     @GetMapping("/cadastro")
     public String getCadastro(Model model) {
-
         Cliente cliente = new Cliente();
-
         model.addAttribute("cliente", cliente);
-
         return "conta/cadastro";
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestParam String identificador, @RequestParam String senha) {
         Cliente clienteResposta = null;
-
         List<Cliente> clientes = clienteService.getAllClientes();
-
         for (Cliente cliente : clientes) {
             if (cliente.getEmail().equals(identificador) && cliente.getSenha().equals(senha)) {
                 clienteResposta = cliente;
@@ -68,10 +61,26 @@ public class ClienteController {
     }
 
     @GetMapping("/meuPerfil")
-    public String getPerfil(){
-
-
+    public String getPerfil() {
         return "banco/meuPerfil";
+    }
+
+    @GetMapping("/perfil/{id}")
+    public ResponseEntity<Cliente> getCliente(@PathVariable String id) {
+        Optional<Cliente> cliente = clienteService.getClienteById(id);
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get()); // Retorna o cliente como JSON
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // Retorna 404 se o cliente não for encontrado
+    }
+
+    // Endpoint POST para receber os dados do cliente e salvar no banco de dados
+    @PostMapping("/perfil")
+    public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente) {
+        // Salva o cliente no banco de dados
+        Cliente clienteSalvo = clienteService.createCliente(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteSalvo); // Retorna o cliente criado com o status
+                                                                             // 201
     }
 
     @GetMapping("/pagamento/pagar")
---
+++ app_adapter/src/main/resources/static/js/perfil.js
@@ -0,0 +1,9 @@
+
+async function getPerfil() {
+    const dadosUsuario = JSON.parse(localStorage.getItem('dadosUsuario'));
+localStorage.setItem('dadosUsuario', JSON.stringify(dadosUsuario));
+}
+
+
+
+
